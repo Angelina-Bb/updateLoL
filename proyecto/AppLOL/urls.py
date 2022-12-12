93b9b7b554f12ca57f50dfba6083d52bc2eb6782
arielfsz <arielf95@hotmail.com>
2022-12-12 18:37:48 -0300

    arreglo de URLs

--- proyecto/AppLOL/urls.py
+++ proyecto/AppLOL/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from AppLOL import views
 
 urlpatterns = [
-    path("", views.inicio),
-    path("about/", views.about),
-    path("actualizaciones/", views.updates),
-    path("campeones/", views.campeones),
-    path("comunidad/", views.comunidad),
-    path("chat/", views.chat),
-    path("login/", views.login_request),
+    path("", views.inicio, name="inicio"),
+    path("about/", views.about, name="about"),
+    path("actualizaciones/", views.updates, name="updates"),
+    path("campeones/", views.campeones, name="campeones"),
+    path("comunidad/", views.comunidad, name="comunidad"),
+    path("chat/", views.chat, name="chat"),
+    path("accounts/login/", views.login_request, name="login"),
+    path("accounts/register/", views.register, name="register"),
+    path("accounts/profile/", views.profile, name="profile"),
 ]
